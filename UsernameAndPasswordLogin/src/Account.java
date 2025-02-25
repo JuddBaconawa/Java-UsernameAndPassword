3dc16e0756f1ff73f18b25069059cfc07f4ae082
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-02-25 18:17:15 -0500

    Account.jva class created and populated
    declaration for an "Account" and its attributes are initialized.

---
+++ UsernameAndPasswordLogin/src/Account.java
@@ -0,0 +1,18 @@
+//packages
+
+
+//imports
+
+
+//Account class
+public class Account {
+	
+	//declaration/initialization of an Account's attributes
+	private String username;
+	private char[] password;
+	private int userId;
+
+
+
+
+}
