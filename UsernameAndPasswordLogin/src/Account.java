4770893b06b75e9d03e84072fcc712d309383391
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-13 11:43:00 -0400

    Update Account.java
    Comment added on Account.java

--- UsernameAndPasswordLogin/src/Account.java
+++ UsernameAndPasswordLogin/src/Account.java
@@ -1,46 +1,36 @@
 //packages
 
-
 //imports
 public class Account {
 
-
-	
-	//declaration/initialization of an Account's attributes
-	private String username; //static removed because it makes all credentials share the same value
+	// declaration/initialization of an Account's attributes
+	private String username; // static removed because it makes all credentials share the same value
 	private char[] password;
-	private int userId; //TBA
+	private int userId; // TBA
 
-
-
-	//account method constructor
+	// account method constructor
 	public Account(String username, String password, int userId) {
 
-		//sets the username used
-		this.username = username; //normal - this.username
-		this.password = password.toCharArray(); //changed from password to password.toCharArray()
-		this.userId = userId; //just adding now for the future
+
 
 	}
 
-	//username getter
+	// username getter
 	public String getUsername() {
-		//returns username
+		// returns username
 		return username;
 	}
 
-	//password getter
+	// password getter
 	public char[] getPassword() {
-		//returns password
+		// returns password
 		return password;
 	}
 
-	//userId Getter
+	// userId Getter
 	public int getUserId() {
-		//returns userId
+		// returns userId
 		return userId;
 	}
-
-
 
 }
